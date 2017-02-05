47cea9267527205d82d45a2fb01bed48a3af4fdd
Thomas Countz <thomascountz@gmail.com>
2017-02-05 00:03:27 +0000

    added address present validation spec

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe EmailAddress, type: :model do
   it 'is valid' do
     expect(email_address).to be_valid
   end
+  
+  it 'is invalid without an address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
 end
