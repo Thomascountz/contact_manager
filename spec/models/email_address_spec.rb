c20eb4a5494832c135841f66f49e70848017760a
Thomas Countz <thomascountz@gmail.com>
2017-02-05 00:01:21 +0000

    added valid email address spec

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) { EmailAddress.new }
+  
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
 end
